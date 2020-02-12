f6905f62c05a87714219552f5cba0bc991bc3f02
Matteo Rebosolan <m.rebosolan@student.tudelft.nl>
2020-02-12 11:12:54 +0100

    Data point reader

---
+++ Data Point Reader.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+fin = open("aerodynamicloada320 (1).dat")
+data_points = []
+
+for line in fin:
+    row = []
+    line = line.split(",")
+    for value in line:
+        row.append(float(value))
+    data_points.append(np.array(row))
+
+data_points = np.array(data_points)
+
+print(data_points)
+
+
