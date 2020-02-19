58deb56c15ff363125e4f73ed28b658116d23017
Maciek <M.R.Jankowski@student.tudelft.nl>
2020-02-19 09:44:04 +0100

    msg

---
+++ interpolation.py
@@ -0,0 +1,83 @@
+import DataPointReader
+import loadfunction
+from functools import reduce
+import operator
+import numpy as np
+import math as m
+from matplotlib import pyplot
+import sympy
+value, x, z = loadfunction.coordinates()
+class interpolation:
+
+    def __init__(self):
+        self.data = interpolation.readerx()
+
+
+    def readerx():
+        fin = open("aerodynamicloada320.dat")
+        data_points = []
+
+        for line in fin:
+            row = []
+            line = line.split(",")
+            for value in line:
+                row.append(float(value))
+            data_points.append(np.array(row))
+
+        data_points = np.array(data_points)
+
+        return data_points
+    def zdirection(self,l):
+        valuesz = np.array(value[l::40])
+        return valuesz
+    def zdircord(self,l):
+        zcord = []
+        zcord1 = np.array(z[l::40])
+
+
+        return zcord1
+
+
+    def basis(self,x,j,m,l):
+        b = (x - self.zdircord(l)[m]) / (self.zdircord(l)[j] - self.zdircord(l)[m])
+
+        return b
+    def interpolate(self,x,l):
+        y = 0
+        for j in range(80):
+            t = 1
+            for m in range(80):
+                if m != j:
+                    t = t*self.basis(x,j,m,l)
+            y += t*self.zdirection(l)[j]
+        return y
+
+
+        #return np.sum([[np.dot(self.basis(x,j),self.zdirection()[j])]for j in range(1,41)])
+
+
+
+i = interpolation()
+print(len(i.zdircord(1)))
+print(len(i.zdirection(1)))
+#X = i.zdircord(l)
+#print(X)
+#print(i.zdirection(l))
+ill = np.linspace(0,-0.5456,162)
+y = []
+list = []
+#for l in range(41):
+    #print(l)
+    #list.append(l)
+l = 10
+for element in ill:
+    y.append(i.interpolate(element,l))
+
+pyplot.plot(ill,y)
+pyplot.show()
+
+
+
+
+
+
