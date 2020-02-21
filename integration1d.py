0dc5adc944371dc248d007830ac23158471d726c
Maciek <M.R.Jankowski@student.tudelft.nl>
2020-02-21 15:19:38 +0100

    msg2

--- integration1d.py
+++ integration1d.py
@@ -1,19 +1,19 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from interpolation1d import interpolation
 def multiplicator(loadfunction):
     xprime1 = 0
-    xprime2 = 0.5464859422096491
+    xprime2 = -0.5464859422096491
     xprimes = np.linspace(xprime1, xprime2, pointnum)
-    xhinge = (0.225)/2
+    xhinge = (-0.225)/2
     newfunction = []
     for c, element in enumerate(loadfunction):
-        new = element*(xhinge - xprimes[c])
+        new = element*( xhinge - xprimes[c])
         newfunction.append(new)
     return newfunction
 
-def numerical_integrator1d(loadfunction,pointnum):
+def numerical_integrator1d(loadfunction,pointnum,xprime2):
     xprime1 = 0
-    xprime2 = 0.5464859422096491
     xprimes = np.linspace(xprime1, xprime2, pointnum)  # array of x coordinates
 
     tot = 0
@@ -26,7 +26,15 @@ pointnum = 400
 pointnum2 = pointnum - 1
 i = interpolation()
 ill = np.linspace(0,-0.5456,pointnum)
-y = i.interpolate(ill,20)
-print(multiplicator(y))
-integral = numerical_integrator1d(y,pointnum)
-print(integral)
+magni = []
+def resultandtorques():
+    resultants = []
+    torques = []
+    for g in range(41):
+        y = i.interpolate(ill,g,'span',magni)
+        integral = numerical_integrator1d(y,pointnum,-0.5464859422096491)
+        y2 = multiplicator(y)
+        twistint = numerical_integrator1d(y2,pointnum,-0.5464859422096491)
+        resultants.append(integral)
+        torques.append(twistint)
+    return resultants,torques
