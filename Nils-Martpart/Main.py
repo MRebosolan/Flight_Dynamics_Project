e227b0b0e2d050e27009120c3748034b38e5685f
MartvandeVen98 <martvandeven98@hotmail.com>
2020-02-22 13:15:11 +0100

    Update Main.py

--- Nils-Martpart/Main.py
+++ Nils-Martpart/Main.py
@@ -20,7 +20,7 @@ import sys, os
 sys.path.clear()
 sys.path.append(os.path.realpath('..\\svvproject'))
 sys.path.append(os.path.realpath('..\\Nils-Martpart'))
-from Mart_function_definitions import normal_stress_x_bending_function, redundant_shear_flow, shear_centre
+from Mart_function_definitions import normal_stress_x_bending_function, redundant_shear_flow, shear_centre, normal_stress_x_bending_function, von_mises_stress_function
 from definitions_max_stress_deflections import relation_shear_1_and_2_torque_and_torque, alternative_q_base_top1, alternative_q_base_spar, alternative_q_base_bottom1, alternative_q_base_sparA, alternative_q_base_top2, alternative_q_base_bottom2, alternative_q_base_sparB
 
 "--------------------------------------------------------------------------------------------------------------------------"
@@ -66,6 +66,17 @@ for torque in torque_x_direction:
 
 MOI_y_prime = moment_of_inertia_y
 MOI_z_prime = moment_of_inertia_z
+
+maximum_stress_s1 = []
+location_maximum_stress_s1 = []
+maximum_stress_s2 = []
+location_maximum_stress_s2 = []
+maximum_stress_s3 = []
+location_maximum_stress_s3 = []
+maximum_stress_s4 = []
+location_maximum_stress_s4 = []
+maximum_stress_s5 = []
+location_maximum_stress_s5 = []
 
 for j in range(0, len(shear_force_y_x_direction)-1):
     shear_force_y = shear_force_y_x_direction[j]
@@ -157,15 +168,102 @@ for j in range(0, len(shear_force_y_x_direction)-1):
 
     q_total_list_spar = q_total_list_spar_B+q_total_list_spar_A
     s5_list = [sB_list + sA_list]
-"This part will calculate the bending stress combined with the shear stresses for every s-section, all clockwise defined."
-"s1 is the top circular part"
-"s2 is the top triangular part"
-"s3 is the bottom circular part"
-"s4 is the bottom triangular part"
-"s5 is the spar, bottom to top"
 
+    "This part will define geometry for all of the 5 parts, creating a z-list and y-list next to the already existing s-lists."
+    "s1 is the top circular part"
+    "s2 is the top triangular part"
+    "s3 is the bottom circular part"
+    "s4 is the bottom triangular part"
+    "s5 is the spar, bottom to top"
     
+    s1_y_list = []
+    s1_z_list = []
+    for s1 in s1_list:
+        s1_y_list.append(sin(s1))
+        s1_z_list.append(-cos(s1))
+        
+    s2_y_list = []
+    s2_z_list = []
+    for s2 in s2_list:
+        s2_y_list.append(aileron_height/2-s2*sin(aileron_angle_radians))
+        s2_z_list.append(s2*cos(aileron_angle_radians))
+        
+    s3_y_list = []
+    s3_z_list = []
+    for s3 in s3_list:
+        s3_y_list.append(-s3*sin(aileron_angle_radians))
+        s3_z_list.append((chord_length-0.5*aileron_height)-s3*cos(aileron_angle_radians))
+        
+    s4_y_list = []
+    s4_z_list = []
+    for s4 in s4_list:
+        s4_y_list.append(-aileron_height/2+cos(s4))
+        s4_z_list.append(-sin(s4))
+        
+    s5_y_list = []
+    s5_z_list = []
+    for s5 in s5_list:
+        s5_y_list.append(-aileron_height/2+s5)
+        s5_z_list.append(0)
+        
+    "This part will calculate, for every section, the stress throughout. It will return the maximum stress."
     
+    maximum_stress_stored_s1 = 0
+    for i in range(len(s1_list)):
+        bending_stress = normal_stress_x_bending_function(moment_about_z_x_direction[j], moment_of_inertia_y, s1_y_list[i]-centroid_y, moment_about_y_x_direction[j], moment_of_inertia_z, s1_z_list[i]-centroid_z)
+        shear_stress = q_total_list_s1[i]*skin_thickness
+        von_mises_stress = von_mises_stress_function(bending_stress, shear_stress)
+        if von_mises_stress > maximum_stress_stored_s1:
+            maximum_stress_stored_s1 = von_mises_stress
+            location_maximum_stress_s1 = s1_list[i]
+            
+    maximum_stress_stored_s2 = 0
+    for i in range(len(s2_list)):
+        bending_stress = normal_stress_x_bending_function(moment_about_z_x_direction[j], moment_of_inertia_y, s2_y_list[i]-centroid_y, moment_about_y_x_direction[j], moment_of_inertia_z, s2_z_list[i]-centroid_z)
+        shear_stress = q_total_list_s2[i]*skin_thickness
+        von_mises_stress = von_mises_stress_function(bending_stress, shear_stress)
+        if von_mises_stress > maximum_stress_stored_s1:
+            maximum_stress_stored_s2 = von_mises_stress
+            location_maximum_stress_s2 = s2_list[i]
+            
+    maximum_stress_stored_s3 = 0
+    for i in range(len(s3_list)):
+        bending_stress = normal_stress_x_bending_function(moment_about_z_x_direction[j], moment_of_inertia_y, s3_y_list[i]-centroid_y, moment_about_y_x_direction[j], moment_of_inertia_z, s3_z_list[i]-centroid_z)
+        shear_stress = q_total_list_s3[i]*skin_thickness
+        von_mises_stress = von_mises_stress_function(bending_stress, shear_stress)
+        if von_mises_stress > maximum_stress_stored_s1:
+            maximum_stress_stored_s3 = von_mises_stress
+            location_maximum_stress_s3 = s3_list[i] 
+            
+    maximum_stress_stored_s4 = 0
+    for i in range(len(s4_list)):
+        bending_stress = normal_stress_x_bending_function(moment_about_z_x_direction[j], moment_of_inertia_y, s4_y_list[i]-centroid_y, moment_about_y_x_direction[j], moment_of_inertia_z, s4_z_list[i]-centroid_z)
+        shear_stress = q_total_list_s4[i]*skin_thickness
+        von_mises_stress = von_mises_stress_function(bending_stress, shear_stress)
+        if von_mises_stress > maximum_stress_stored_s4:
+            maximum_stress_stored_s4 = von_mises_stress
+            location_maximum_stress_s4 = s4_list[i]
+            
+    maximum_stress_stored_s5 = 0
+    for i in range(len(s5_list)):
+        bending_stress = normal_stress_x_bending_function(moment_about_z_x_direction[j], moment_of_inertia_y, s5_y_list[i]-centroid_y, moment_about_y_x_direction[j], moment_of_inertia_z, s5_z_list[i]-centroid_z)
+        shear_stress = q_total_list_s5[i]*skin_thickness
+        von_mises_stress = von_mises_stress_function(bending_stress, shear_stress)
+        if von_mises_stress > maximum_stress_stored_s5:
+            maximum_stress_stored_s5 = von_mises_stress
+            location_maximum_stress_s5 = s5_list[i]     
+            
+    "This last part will store everything"
+    maximum_stress_s1.append(maximum_stress_stored_s1)
+    location_maximum_stress_s1.append(location_maximum_stress_s1)
+    maximum_stress_s2.append(maximum_stress_stored_s2)
+    location_maximum_stress_s2.append(location_maximum_stress_s2)
+    maximum_stress_s3.append(maximum_stress_stored_s3)
+    location_maximum_stress_s3.append(location_maximum_stress_s3)
+    maximum_stress_s4.append(maximum_stress_stored_s4)
+    location_maximum_stress_s4.append(location_maximum_stress_s4)
+    maximum_stress_s5.append(maximum_stress_stored_s5)
+    location_maximum_stress_s5.append(location_maximum_stress_s5)    
 "--------------------------------------------------------------------------------------------------------------------------"
 "This part will handle the computation of shear centre and following that the deflection due to torque"
 q_total_top1 = q_total_list_top_1[1]
