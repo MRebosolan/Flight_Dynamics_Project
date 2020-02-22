ce3790c884e16e5964cda72abeb6ac4a4e77f98a
MartvandeVen98 <martvandeven98@hotmail.com>
2020-02-22 12:07:17 +0100

    joe

--- Nils-Martpart/Main.py
+++ Nils-Martpart/Main.py
@@ -14,13 +14,13 @@ Created on Wed Feb 19 11:42:44 2020
 "--------------------------------------------------------------------------------------------------------------------------"
 "This section imports stuff, mainly the definitions of functions can then be used"
 
-import math
+import math 
 import numpy as np
 import sys, os
 sys.path.clear()
 sys.path.append(os.path.realpath('..\\svvproject'))
 sys.path.append(os.path.realpath('..\\Nils-Martpart'))
-from Mart_function_definitions import normal_stress_x_bending_function, redundant_shear_flow
+from Mart_function_definitions import normal_stress_x_bending_function, redundant_shear_flow, shear_centre
 from definitions_max_stress_deflections import relation_shear_1_and_2_torque_and_torque, alternative_q_base_top1, alternative_q_base_spar, alternative_q_base_bottom1, alternative_q_base_sparA, alternative_q_base_top2, alternative_q_base_bottom2, alternative_q_base_sparB
 
 "--------------------------------------------------------------------------------------------------------------------------"
@@ -42,8 +42,8 @@ spar_thickness = 0.005
 chord_length = 5
 integral_step = 0.01
 aileron_radius = aileron_height/2
-aileron_angle_radians = 1/5*pi
-area_circular_section = 0.5*pi*aileron_radius^2
+aileron_angle_radians = 1/5*math.pi
+area_circular_section = 0.5*math.pi*aileron_radius**2
 area_triangular_section = 0.5*aileron_height*(chord_length-aileron_height/2)
 
 "--------------------------------------------------------------------------------------------------------------------------"
@@ -99,7 +99,7 @@ for j in range(0, len(shear_force_y_x_direction)-1):
     y_pos = 0
     q_base_list_spar_area_2_B, sB_list = alternative_q_base_sparB(shear_force_y, shear_force_z, MOI_y_prime, MOI_z_prime, chord_length, integral_step, aileron_height, skin_thickness, y_pos)
 
-"The upcoming part adds the base flows of the previous parts to the current part"
+    "The upcoming part adds the base flows of the previous parts to the current part"
     
     for i in range(0, len(q_base_list_spar_area_1)-1):
         q_base_list_spar_area_1[i] = q_base_list_spar_area_1[i]+q_base_list_top_1[-1]
@@ -116,7 +116,7 @@ for j in range(0, len(shear_force_y_x_direction)-1):
     for i in range(0, len(q_base_list_spar_area_2_B)-1):
         q_base_list_spar_area_2_B[i] = q_base_List_spar_area_2_B[i]+q_base_list_bottom_2[-1]
 
-"The upcoming part adds the shear flows of the spars together to get to one value"
+    "The upcoming part adds the shear flows of the spars together to get to one value"
     
     q_base_list_spar_A = []
     for i in range(0, len(q_base_list_spar_area_2_A)-1):
@@ -126,11 +126,11 @@ for j in range(0, len(shear_force_y_x_direction)-1):
     for i in range(0, len(q_base_list_spar_area_2_B)-1):
         q_base_list_spar_B.append(q_base_list_spar_area_2_B[i]+q_base_list_spar_area_1[-i])
         
-"The upcoming part calculates the redundant shear flows for the sections"
+        "The upcoming part calculates the redundant shear flows for the sections"
 
     redundant_shear_flow_circular_section, redundant_shear_flow_triangular_section = redundant_shear_flow(q_base_list_top_1, s1_list, q_base_list_bottom_2, s4_list, aileron_radius, aileron_angle_radians, aileron_height, area_circular_section, area_triangular_section, ratio_between_redundant_shear_flows)
 
-"The upcoming part adds the redundant, base and torque shear flows"    
+    "The upcoming part adds the redundant, base and torque shear flows"    
     q_total_list_top_1 = []    
     for i in range(0, len(q_base_list_top_1)-1):
         q_total_list_top_1.append(q_base_list_top_1[i]+redundant_shear_flow_circular_section+shear_flow_torque_circular_part_list[j])
@@ -168,8 +168,15 @@ for j in range(0, len(shear_force_y_x_direction)-1):
     
 "--------------------------------------------------------------------------------------------------------------------------"
 "This part will handle the computation of shear centre and following that the deflection due to torque"
+q_total_top1 = q_total_list_top_1[1]
+q_total_bottom1 = q_total_list_bottom_1[1]
+q_total_top2 = q_total_list_top_2[1]
+q_total_bottom2 = q_total_list_bottom_2[1]
 
 
+shear_centre_location_wrt_spar = shear_centre(q_total_top1, q_total_bottom1, q_total_top2, q_total_bottom2, aileron_height)
+
+print(shear_centre_location_wrt_spar)
 
 "--------------------------------------------------------------------------------------------------------------------------"
 "This part will cover the bending equations and deflection due to bending"
