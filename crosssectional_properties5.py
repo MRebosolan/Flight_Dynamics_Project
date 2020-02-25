7f7b542a991de51f5f431c1cef8b4a5d997ae211
lizlo12 <lizzymiddendorp@hotmail.com>
2020-02-25 18:01:15 +0100

    Update crosssectional_properties5.py

--- crosssectional_properties5.py
+++ crosssectional_properties5.py
@@ -7,74 +7,82 @@ import numpy as np
 import math as m
 import matplotlib.pyplot as plt
 
-Ca = 0.547  # m
-la = 2.771  # m
-x1 = 0.153  # m
-x2 = 1.281  # m
-x3 = 2.681  # m
-xa = 0.28   # m
-ha = 0.225  # m
+aircraft = "B737" # Write either A320, F100, CRJ700 or Do228 (bear in mind capitals); this is used for aerodynamic loading
+Ca = 0.605  # m
+la = 2.661  # m
+x1 = 0.172  # m
+x2 = 1.211  # m
+x3 = 2.591  # m
+xa = 0.35   # m
+ha = 0.205  # m
 tsk = 1.1/1000  # m
-tsp = 2.9/1000  # m
+tsp = 2.8/1000  # m
 tst = 1.2/1000  # m
-hst = 15./1000   # m
-wst = 20./1000   # m
-nst = 17 # -
-d1 = 0.01103  # m
-d3 = 0.01642  # m
-theta = m.radians(26)  # rad
-P = 91.7*1000  # N
-
+hst = 16./1000   # m
+wst = 19./1000   # m
+nst = 15  # -
+d1 = 0.01154  # m
+d3 = 0.01840  # m
+theta = m.radians(28)  # rad
+P = 97.4*1000  # N
 "airfoil plot"
 R=ha/2          #radius 
 length=m.pi*R**2
 
 Ab=wst*tst+(hst-tst)*tst #m
 l=2*m.sqrt((Ca-R)**2+(R)**2)+m.pi*R
-b=l/17
+b=l/nst
 k=b/R #radians
 
 z_coordinates=[]
 y_coordinates=[]
+z_spar=[]
+y_spar=[]
 A_st=(hst)*tst+tst*wst
 
 "Stringer placement"
 z_stringer=[] 
 y_stringer=[]
 stringer_array=[]
-for n in range(0,100000):
+z_stringer.append(-R)
+y_stringer.append(0)
+stringer_array.append([A_st, 0,-R])
+h=0
+for n in range(0,10001):
     z=-R*m.cos(n*b/(R))
     y=R*m.sin(n*b/(R))
-    if n>3:
-        last_room=0.5*m.pi*R-2*b
-        z1=0
-        z2=(n-2)*(Ca-R)/100000
-        y1=R
-        y2=-R/(Ca-R)*z2+R
-        l_p=m.sqrt((z1-z2)**2+(y1-y2)**2)
-        z=z2
-        y=y2
-        for i in range (0,10):
-            if abs(l_p-(i*b-last_room))<0.000002:
-                z_stringer.append(z)
-                z_stringer.append(z)
-                y_stringer.append(y)
-                y_stringer.append(-y)
-                stringer_array.append([A_st, y,z])
-                stringer_array.append([A_st, -y,z])
-            elif z>(Ca-R):
-                break
-    elif n<3:
+    if z<=0. and z>-R+0.01:
+        z_stringer.append(z)
         z_stringer.append(z)
         y_stringer.append(y)
-        stringer_array.append([A_st, y,z])      
-        if n>0:
-            z_stringer.append(z)
-            y_stringer.append(-y)
-            stringer_array.append([A_st, -y,z])
-
-
-       
+        y_stringer.append(-y)
+        stringer_array.append([A_st, y,z])
+        stringer_array.append([A_st, -y,z])
+        h=h+2
+    elif z>0:
+        break
+for n in range(0,10001):
+     last_room=0.5*m.pi*R-(h/2)*b
+     z1=0
+     z2=(n-2)*(Ca-R)/10000
+     y1=R
+     y2=-R/(Ca-R)*z2+R
+     l_p=m.sqrt((z1-z2)**2+(y1-y2)**2)
+     z=round(z2,6)
+     y=round(y2,6)
+     for i in range (0,nst):
+         if abs(l_p-(i*b-last_room))<=0.000025:
+             z_stringer.append(z)
+             z_stringer.append(z)
+             y_stringer.append(y)
+             y_stringer.append(-y)
+             stringer_array.append([A_st, y,z])
+             stringer_array.append([A_st, -y,z])
+         elif z>(Ca-R):
+                 break
+   
+stringers=np.array(stringer_array)
+print(stringers)   
 "airfoil placement"
 for i in range(0,101):
     z=-R+R*i/(100)
@@ -84,8 +92,8 @@ for i in range(0,101):
 for i in range(-100,101):
     z=0
     y=i/100*R
-    z_coordinates.append(z)
-    y_coordinates.append(y)
+    z_spar.append(z)
+    y_spar.append(y)
 for i in range(0,101):
     z=i*(Ca-R)/100
     y=-R/(Ca-R)*z+R
@@ -109,7 +117,7 @@ A_sk1=m.pi*R*tsk
 A_sk2=m.sqrt((Ca-R)**2+R**2)*tsk
 A_sp=ha*tsp
 
-A_tot=A_sk1+2*A_sk2+A_sp+17*A_st
+A_tot=A_sk1+2*A_sk2+A_sp+nst*A_st
 print("Total area:", A_tot, "m^2")
 
 Az_st=A_st*sum(z_stringer) #m^2
@@ -122,13 +130,16 @@ Ay_sk3=A_sk2*-1/2*(R)
 zc=(Az_st+Az_sk1+2*Az_sk2)/A_tot
 yc=(Ay_st+Ay_sk1+Ay_sk2+Ay_sk3)/A_tot          #due to symmetry it should be 0
 print("centroid",zc,yc)
-
+print(l)
 "airfoil plot"
-plt.plot(z_coordinates,y_coordinates, color='black')
+
+plt.plot(z_coordinates,y_coordinates, color='black',label="Periphery")
+plt.plot(z_spar,y_spar,color='green', label="Spar")
 plt.xlim(-0.2,0.6)
 plt.ylim(-0.2,0.3)
-plt.scatter(z_stringer,y_stringer, s=30, color='red')
-plt.scatter(zc, yc, marker="x", color='orange')
+plt.scatter(z_stringer,y_stringer, s=30, color='red',label="Stringers")
+plt.scatter(zc, yc, marker="x", color='orange',label="Centroid")
+plt.legend()
 
 "Moment of Inertia results"
 #Calculate moment of inertia of straght skin parts
