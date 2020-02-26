acd7adbfe89a16bda8ac8d07542e127d339e60e0
lizlo12 <lizzymiddendorp@hotmail.com>
2020-02-26 18:27:39 +0100

    Update crosssectional_properties5.py

--- crosssectional_properties5.py
+++ crosssectional_properties5.py
@@ -7,24 +7,25 @@ import numpy as np
 import math as m
 import matplotlib.pyplot as plt
 
-aircraft = "B737" # Write either A320, F100, CRJ700 or Do228 (bear in mind capitals); this is used for aerodynamic loading
-Ca = 0.605  # m
-la = 2.661  # m
-x1 = 0.172  # m
-x2 = 1.211  # m
-x3 = 2.591  # m
-xa = 0.35   # m
-ha = 0.205  # m
+aircraft = "A320" # Write either A320, F100, CRJ700 or Do228 (bear in mind capitals); this is used for aerodynamic loading
+Ca = 0.547  # m
+la = 2.771  # m
+x1 = 0.153  # m
+x2 = 1.281  # m
+x3 = 2.681  # m
+xa = 0.28   # m
+ha = 0.225  # m
 tsk = 1.1/1000  # m
-tsp = 2.8/1000  # m
+tsp = 2.9/1000  # m
 tst = 1.2/1000  # m
-hst = 16./1000   # m
-wst = 19./1000   # m
-nst = 15  # -
-d1 = 0.01154  # m
-d3 = 0.01840  # m
-theta = m.radians(28)  # rad
-P = 97.4*1000  # N
+hst = 15./1000   # m
+wst = 20./1000   # m
+nst = 17 # -
+d1 = 0.01103  # m
+d3 = 0.01642  # m
+theta = m.radians(26)  # rad
+P = 91.7*1000  # N
+
 "airfoil plot"
 R=ha/2          #radius 
 length=m.pi*R**2
