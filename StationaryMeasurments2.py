b495cf2d71f10181715039ef01e1fb66859b75f1
lizlo12 <lizzymiddendorp@hotmail.com>
2020-03-12 16:13:56 +0100

    hi

--- StationaryMeasurments2.py
+++ StationaryMeasurments2.py
@@ -44,14 +44,6 @@ def valueimport2(row1,row2,col,file2):
     for i in range(row1,row2+1):
         file2.append(float(file1[i][col]))
     return file2
-
-
-
-
-
-
-
-
 IAS=[]
 IAS=valueimport2(26,31,4,IAS)
 AOA=[]
@@ -59,10 +51,14 @@ AOA=valueimport2(26,31,5,AOA)
 "Transform angle of attack to radians"
 
 def degtorad(angledeg):
-    anglerad=angledeg*m.pi/180
-    return(anglerad)
-angle_of_attack=np.array(AOAtoRad(AOA))
-print(angle_of_attack[0])
+    aoa=[]
+    for i in angledeg:
+        anglerad=i*m.pi/180
+        aoa.append(anglerad)
+    return(aoa)
+
+angle_of_attack0=np.array(degtorad(AOA))
+
 alpha_1 = 1.7 * m.pi / 180 #degree to radians
 alpha_2 = 2.4 * m.pi / 180 #degree to radians
 alpha_3 = 3.6 * m.pi / 180 #degree to radians
@@ -142,6 +138,7 @@ def mach(p0,p,rho0,V,lambda_1):
     M=m.sqrt((2/(1-lambda_1))*((1 + p0/p1 * ((1 + (1-lambda_1)/(2*lambda_1)*rho0/p0*V_c1**2)**(lambda_1/(1-lambda_1))-1))**((1-lambda_1)/lambda_1) -1))
     return M
 M1 = m.sqrt((2/0.4)*((1 + p0/p1 * ((1 + 0.4/2.8 *rho0/p0*V_c1**2)**(1.4/0.4)-1))**(0.4/1.4) -1))
+print(M1)
 M2 = m.sqrt((2/0.4)*((1 + p0/p2 * ((1 + 0.4/2.8 *rho0/p0*V_c2**2)**(1.4/0.4)-1))**(0.4/1.4) -1))
 M3 = m.sqrt((2/0.4)*((1 + p0/p3 * ((1 + 0.4/2.8 *rho0/p0*V_c3**2)**(1.4/0.4)-1))**(0.4/1.4) -1))
 M4 = m.sqrt((2/0.4)*((1 + p0/p4 * ((1 + 0.4/2.8 *rho0/p0*V_c4**2)**(1.4/0.4)-1))**(0.4/1.4) -1))
@@ -160,7 +157,7 @@ TAT5 = 6 + 273.15 #Celsius to Kelvin
 TAT6 = 5.2 + 273.15 #Celsius to Kelvin
 
 "Converting total temperature to normal temperature"
-def normTemp(TAT,M)
+def normTemp(TAT,M):
     T=TAT/(1+0.2*M**2)
     return T
 T1 = TAT1 / (1+0.2*M1**2)
@@ -206,6 +203,7 @@ rho6 = p6/(R*T6)
 "Caclulate equivalent airspeed"
 def EAS(TAS,rho,rho0):
     EAS=TAS*m.sqrt(rho/rho0)
+    return EAS
 EAS1 = TAS1 * m.sqrt(rho1/rho0)
 EAS2 = TAS2 * m.sqrt(rho2/rho0)
 EAS3 = TAS3 * m.sqrt(rho3/rho0)
@@ -225,7 +223,7 @@ lift_coefficients = [lift_coefficient_1, lift_coefficient_2, lift_coefficient_3,
 
 "Plot CL-alpha figure"
 
-x = angle_of_attack
+x = np.array(angle_of_attack)
 y = np.array(lift_coefficients)
 plt.plot(x, y, 'o', color='red', label='Measuring point')
 mo, bo = np.polyfit(x, y, 1)
@@ -244,16 +242,14 @@ print("Alpha_CL=0", zero_lift_angle_of_attack)
 print('CL_alpha:', mo)
 
 "Calculate thrust"
-def thrust():
-    Tp1 = 3678.46 + 3784.69
-    Tp2 = 3007.55 + 3069.61
-    Tp3 = 2410.92 + 2537.7
-    Tp4 = 1873.55 + 2026.55
-    Tp5 = 1903.04 + 2087.09
-    Tp6 = 2220.99 +2418.08
-    Tp=np.array([Tp1,Tp2,Tp3,Tp4,Tp5,Tp6])
-    return(Tp)
-Tp=thrust()
+Tp1 = 3678.46 + 3784.69
+Tp2 = 3007.55 + 3069.61
+Tp3 = 2410.92 + 2537.7
+Tp4 = 1873.55 + 2026.55
+Tp5 = 1903.04 + 2087.09
+Tp6 = 2220.99 +2418.08
+Tp=[Tp1,Tp2,Tp3,Tp4,Tp5,Tp6]
+
 "Calculate drag"
 def drag(angle,Thrust):
     Drag=[]
@@ -261,15 +257,19 @@ def drag(angle,Thrust):
         D=Tp[i]
         Drag.append(D)
     return(Drag)
-Drag=np.array(drag(angle_of_attack,Tp))
-
+D1 = Tp1*np.cos(alpha_1)
+D2 = Tp2*np.cos(alpha_2)
+D3 = Tp3*np.cos(alpha_3)
+D4 = Tp4*np.cos(alpha_4)
+D5 = Tp5*np.cos(alpha_5)
+D6 = Tp6*np.cos(alpha_6)
 
 "Caclulate drag coefficient"
-def dragcoeffiecient(drag,rho,TAS,S)
-    CD=drag,rho,TAS,S
-    return(CD)
+def dragcoeffiecient(drag,rho,TAS,S):
+    CD=drag/(0.5*rho*TAS**2*S)
+    return CD
 CD1 = D1 / (0.5*rho1*TAS1**2*S)
-CD2 = D2 / (0.5*rho2*TAS2**2*S),
+CD2 = D2 / (0.5*rho2*TAS2**2*S)
 CD3 = D3 / (0.5*rho3*TAS3**2*S)
 CD4 = D4 / (0.5*rho4*TAS4**2*S)
 CD5 = D5 / (0.5*rho5*TAS5**2*S)
@@ -346,16 +346,21 @@ oswald = 1 / (m.pi * A * dCD_dCL2)
 print('Oswald efficiency factor:', oswald)
 
 "Calculate reduced equivalent airspeed"
-def reducedV(p,rho,T, fuel_used):
+def reducedV(rho,T, fuel_used,IAS,hp):
+    p = p0 * (1 + (lambda0 * hp) / T0) ** (-g0 / (lambda0 * R))
     weight_total = (OEW + weight_fuel + weight_payload - fuel_used) * g0
-    M = m.sqrt((2 / (1 - lambda_1)) * ((1 + p0 / p * ((1 + (1 - lambda_1) / (2 * lambda_1) * rho0 / p0 * V_c1 ** 2) ** (lambda_1 / (1 - lambda_1)) - 1))** ((1 - lambda_1) / lambda_1) - 1))
+    V_c = (IAS - 2) * 0.514444444  # kts to m/s
+    M = m.sqrt((2/0.4)*((1 + p0/p * ((1 + 0.4/2.8 *rho0/p0*V_c**2)**(1.4/0.4)-1))**(0.4/1.4) -1))
     a = m.sqrt(lambda_1 * R * T)
     TAS = M * a
     EAS = TAS * m.sqrt(rho/rho0)
-    Ve_bar1 = EAS * m.sqrt(standard_weight / weight_total)
+    Ve_bar = EAS * m.sqrt(standard_weight/weight_total)
     return Ve_bar
+V_E1=reducedV(rho1,T1,fuel_used1,249,hp1)
+print(V_E1)
 
-
+Ve_bar1 = EAS1 * m.sqrt(standard_weight / weight_total1)
+print(Ve_bar1)
 Ve_bar2 = EAS2 * m.sqrt(standard_weight / weight_total2)
 Ve_bar3 = EAS3 * m.sqrt(standard_weight / weight_total3)
 Ve_bar4 = EAS4 * m.sqrt(standard_weight / weight_total4)
